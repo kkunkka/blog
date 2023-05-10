610a7df0b07a5e8da0b8a53396d0929a26c7a8d6
kkunkka <kkunkka@zohomail.com>
2023-05-10 10:32:17 +0800

    chore: 添加导航页网站

--- data/resource.js
+++ data/resource.js
@@ -3,20 +3,10 @@ const resource = [
 		name: '常用',
 		resources: [
 			{
-				name: 'Github',
-				describe: '最大的软件托管平台',
-				href: 'https://github.com'
-			},
-			{
 				name: 'zoho mail',
 				describe: 'zoho 邮箱',
 				href: 'https://accounts.zoho.com'
 			},
-			{
-				name: 'Github 开源指南',
-				describe: 'Github 开源指南',
-				href: 'https://opensource.guide/'
-			}
 		]
 	},
 	{
@@ -30,8 +20,43 @@ const resource = [
 		]
 	},
 	{
+		name: 'Git',
+		resources: [
+			{
+				name: 'Github',
+				describe: '最大的软件托管平台',
+				href: 'https://github.com'
+			},
+			{
+				name: 'Github Readme 内容生成',
+				describe: 'Github Readme 内容生成',
+				href: 'https://rahuldkjain.github.io/gh-profile-readme-generator/'
+			},
+			{
+				name: 'Github 开源指南',
+				describe: 'Github 开源指南',
+				href: 'https://opensource.guide/'
+			}
+		]
+	},
+	{
+		name: '前端',
+		resources: [
+			{
+				name: 'Vue',
+				describe: '用于构建用户界面的 JavaScript 框架',
+				href: 'https://cn.vuejs.org/'
+			}
+		]
+	},
+	{
 		name: '文档',
 		resources: [
+			{
+				name: 'Docusaurus',
+				describe: '一款静态站点生成器',
+				href: 'https://docusaurus.io/zh-CN/docs'
+			},
 			{
 				name: 'baeldung',
 				describe: 'Java Spring的一个教程网站',
@@ -67,6 +92,21 @@ const resource = [
 				describe: '在线免费抠图',
 				href: 'https://remove.photos/zh-cn'
 			}
+		]
+	},
+	{
+		name: '设计',
+		resources: [
+			{
+				name: 'xicons',
+				describe: '图标库',
+				href: 'https://xicons.org/#/zh-CN'
+			},
+			{
+				name: 'heroicons',
+				describe: '图标库',
+				href: 'https://heroicons.dev/'
+			},
 		]
 	}
 ]
